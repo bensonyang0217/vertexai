8ca226ec55726640eb46a1943d71570b2a9fd12d
Benson Yang <f932037@gmail.com>
2024-07-08 09:12:34 +0800

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+from langchain_google_vertexai import VertexAI
+
+# To use model
+model = VertexAI(model_name="gemini-1.5-pro-001", location="asia-east1")
+message = "1+1 = ?"
+output = model.invoke(message)
+print(output)
