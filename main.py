29a8bb1cf462cc5ba7df3d62a970355631054720
Benson Yang <f932037@gmail.com>
2024-08-06 15:56:33 +0800

    [update] prompt

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from langchain_google_vertexai import VertexAI
 
 # To use model
 model = VertexAI(model_name="gemini-1.5-pro-001", location="asia-east1")
-message = "1+1 = ??"
+message = "周杰倫跟周餅輪的關係"
 output = model.invoke(message)
 print(output)
